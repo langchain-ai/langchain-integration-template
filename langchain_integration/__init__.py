d9e2c620fd634d9fbc39e09f4b1fe05537361fd0
Bagatur <baskaryan@gmail.com>
2023-11-24 13:27:03 -0800

    vector store

--- langchain_integration/__init__.py
+++ langchain_integration/__init__.py
@@ -1,4 +1,5 @@
 from langchain_integration.chat_models import ChatIntegration
 from langchain_integration.llms import Integration
+from langchain_integration.vectorstores import IntegrationVectorStore
 
-__all__ = ["Integration", "ChatIntegration"]
+__all__ = ["Integration", "ChatIntegration", "IntegrationVectorStore"]
